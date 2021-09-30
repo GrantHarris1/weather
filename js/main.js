a42e46dd467bf221da8933b50c1461daa9d2526d
grantharriselectric <grantharriselectric@gmail.com>
2021-09-30 13:52:40 -0400

    first rough save

--- js/main.js
+++ js/main.js
@@ -1,26 +1,31 @@
-const axios = require('axios');
-var data = {};
-function handleRequest(response) {
-    // console.log({ response })
-    return response.json()
+let zipInput = '';
+const url= 'https://api.openweathermap.org/data/2.5/weather?zip=40513&appid=39f71d4438befcf9779a7ef59013d16a';
+
+document.getElementById('sendButton').addEventListener('click',makeGetRequest);
+function makeGetRequest() {
+    let zipInput = document.getElementById('zipInput').value;
+    axios.get(url.replace('40513',zipInput))
+    .then(
+        (response) => {
+            var result = response.data;
+            var t = Math.floor(ktoC(result.main.temp));
+            document.getElementById('temp').innerText=`Current Temp ${t}F`
+            console.log(result);
+            function ktoC(temp){
+                return ((temp-273.15)*1.8)+32;
+                };
+                document.getElementById("con").innerText=`Current coditions are ${result.weather[0].main}`;
+                
+            },
+        (error) => {
+            console.log(error);
+        }
+        
+            
+           
+        
+    );
 }
-function handleJSON(json) {
-    console.log(json)
-let 
-// Make a request for a user with a given ID
-axios.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=39f71d4438befcf9779a7ef59013d16a')
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  data = response
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .then(function () {
-    // always executed
-  });
-  
 
-
+    //  zip= zip.replace('40513','zipInput') ;     
+    
