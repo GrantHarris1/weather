2eaefde2d22feba090cf61ddff2c6c79d7097c20
grantharriselectric <grantharriselectric@gmail.com>
2021-10-01 09:26:37 -0400

    last rough draft

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 let zipInput = '';
 const url= 'https://api.openweathermap.org/data/2.5/weather?zip=40513&appid=39f71d4438befcf9779a7ef59013d16a';
 document.getElementById('sendButton').addEventListener('click',makeGetRequest);
-// document.getElementById("hidden").style.display = "none";
-function makeGetRequest() {
+
+ 
+ function makeGetRequest() {
     let zipInput = document.getElementById('zipInput').value;
     axios.get(url.replace('40513',zipInput))
     .then(
@@ -11,6 +12,7 @@ function makeGetRequest() {
             var t = Math.floor(ktoF(result.main.temp));
             var k = Math.floor(result.main.temp);
             var c = Math.floor(ktoC(result.main.temp));
+            document.getElementById("hidden").style.visibility = "visible";
             document.getElementById('temp').innerText=`${t}(F)`
             console.log(result);
             function ktoF(temp){
@@ -27,16 +29,33 @@ function makeGetRequest() {
                 document.getElementById("city").innerText=`${result.name}`;
                 document.getElementById('tempK').innerText=`${k}(K)`;
                 document.getElementById('tempC').innerText=`${c}(C)`;
+                popimg(t)
             },
         (error) => {
             console.log(error);
         }
+       
         
             
            
         
     );
 }
+function popimg(t){
+    if( t >= 80){
+        pic.src = "img/hot.jpg"
+    }
+    else if (t < 40){
+        pic.src = "img/cold.jpg"
+       }   else {
+        pic.src = "img/mild.jpg"
+        }
+    
+
+}
+
+    
+
 
     //  zip= zip.replace('40513','zipInput') ;     
     
